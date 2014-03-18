63b07e5d0e08cc86a3f9bbf7c311daf69b7ced9b
booklover4 <booklover2010@gmail.com>
2014-03-17 21:38:26 -0500

    First Draft
    Roughly where we're beginning

---
+++ footer.php
@@ -0,0 +1,15 @@
+    </div><!-- #main -->
+ 
+    <div id="footer">
+        <div id="colophon">
+ 
+            <div id="site-info">
+				<p>Copyright here.</p>
+            </div><!-- #site-info -->
+ 
+        </div><!-- #colophon -->
+    </div><!-- #footer -->
+</div><!-- #wrapper -->
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<head profile="http://gmpg.org/xfn/11">
+    <title><?php
+        if ( is_single() ) { single_post_title(); }
+        elseif ( is_home() || is_front_page() ) { bloginfo('name'); print ' | '; bloginfo('description'); get_page_number(); }
+        elseif ( is_page() ) { single_post_title(''); }
+        elseif ( is_search() ) { bloginfo('name'); print ' | Search results for ' . wp_specialchars($s); get_page_number(); }
+        elseif ( is_404() ) { bloginfo('name'); print ' | Not Found'; }
+        else { bloginfo('name'); wp_title('|'); get_page_number(); }
+    ?></title>
+ 
+    <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+ 
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+ 
+    <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+
+    <?php wp_head(); ?>
+ 
+    <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" title="<?php printf( __( '%s latest posts', 'hbd-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
+    <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'hbd-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<?php
+		$content_text_color = get_option('content_text_color');
+		$content_link_color = get_option('content_link_color');
+	?>
+	<style>
+		#content { color:  <?php echo $content_text_color; ?>; }
+		#content a { color:  <?php echo $content_link_color; ?>; }
+	</style>
+</head>
+<body>
+<div id="wrapper" class="hfeed">
+    <div id="header">
+				<div id="name"><h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1></div><div id="login"> <a href="/wp-admin/"> Login </a></div>
+				<div style="margin: 0; height: 30px;"><?php wp_nav_menu( array('menu' => 'Main' )); ?></div>
+          
+    </div><!-- #header -->
+ 
+    <div id="main">
